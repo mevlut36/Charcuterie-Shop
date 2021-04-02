1f4ce1791bb13e8ccc58a9431e3dd700a8c6a211
Mev <36679013+Hirss@users.noreply.github.com>
2021-04-02 14:16:41 +0200

    Add files via upload

---
+++ data_bio.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors','on');
+if(isset($_POST["nom"], $_POST["texte"]) && !empty($_POST["nom"]) && !empty($_POST["texte"])){
+    $nom = htmlspecialchars(addslashes($_POST["nom"]));
+    $texte = htmlspecialchars(addslashes($_POST["texte"]));
+
+    $bdd = new PDO("mysql:host=localhost:3306;dbname=items;charset=utf8", "root", "");
+    $request = $bdd->prepare("INSERT INTO bio (nom, texte, date_time_publication) VALUES ('{$nom}', '{$texte}', NOW())");
+    $request->execute();
+} 
+
+  			$lastid = $bdd->lastInsertId();
+	         
+	         if(isset($_FILES['photo']) AND !empty($_FILES['photo']['name'])) {
+	            if(exif_imagetype($_FILES['photo']['tmp_name']) == 2) {
+	               $chemin = 'upload/'.$lastid.'.jpg';
+	               move_uploaded_file($_FILES['photo']['tmp_name'], $chemin);
+	            } else {
+	               $message = 'Votre image doit être au format jpg';
+	            }
+	         } header("Location:index.php");
+
+?>
