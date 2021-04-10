ee82bf30ef2bef7a3ec5c68844bc7d53f834def3
Mev <36679013+Hirss@users.noreply.github.com>
2021-04-10 15:37:08 +0200

    correction

--- class/Buy.php
+++ class/Buy.php
@@ -28,15 +28,20 @@ class Buy {
     }
 
     public function buy(){
-        $item_name = $this->item('name');
+        $item_name = $this->item['name'];
         $item_query = mysqli_query($this->con, "SELECT price FROM meat WHERE name='$name'");
         $row_item = mysqli_fetch_array($item_query);
 
-        $user_name = $this->user('username');
+        $user_name = $this->user['username'];
         $user_query = mysqli_query($this->con, "SELECT money FROM users WHERE username='$name'");
         $row_user = mysqli_fetch_array($user_query);
 
-        //todo
+        if($this->item['price'] < $this->user['money']){
+            $this->setMoney($this->user["username"], $this->user["money"] - $this->item["price"]);
+            return true;
+        } else {
+            header("Location: index.php");
+        }
     }
 
 }
